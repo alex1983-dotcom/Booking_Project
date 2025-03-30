85b36f51942eb0f1d84117f75658631741ae4076
Макаров Александр <alebarda2018@gmail.com>
2025-03-30 22:34:19 +0300

    настройка маршрутов в приложении bot

--- bot/urls.py
+++ bot/urls.py
@@ -1,7 +1,8 @@
 # bot/urls.py
 from django.urls import path
 from .views import TelegramWebhookView
-
+from .views_bitrix_24 import handle_order  # Импорт обработчика
 urlpatterns = [
     path('webhook/', TelegramWebhookView.as_view(), name='telegram-webhook'),
+    path('api/bitrix24/order/', handle_order, name='handle_order'),
 ]
