63fd98c85385b38b34b77dd6a8406bab3ab119a4
Макаров Александр <alebarda2018@gmail.com>
2025-04-11 17:25:26 +0300

    Создан файл .env для переменных окружения

--- bot/bot_booking/config.py
+++ bot/bot_booking/config.py
@@ -1,17 +1,25 @@
+
 import logging
 from aiogram import Bot
 from aiogram.fsm.storage.memory import MemoryStorage
+from dotenv import load_dotenv
+import os
+
+# Загружаем переменные из файла .env
+load_dotenv()
 
 # Логирование
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Настройки
-TOKEN = "7858593332:AAGhwrIZJsh3ZkhkfgLZ39Sh1GEG2RhpW80"  # Ваш токен
-DJANGO_API_BASE_URL = "http://127.0.0.1:8000/api/booking/"
-
-
+TOKEN = os.getenv("TOKEN")  # Токен из файла .env
+DJANGO_API_BASE_URL = os.getenv("DJANGO_API_BASE_URL")  # Базовый URL API из .env
 
 # Экземпляры бота и хранилища
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
+
+# Проверяем, что токен загружен корректно
+if not TOKEN:
+    logger.error("Токен не найден! Проверьте файл .env.")
